f306cd5e235d83da6af69f87f4c6b55b2f5658f0
Zoryas <gombalesterallen@gmail.com>
2025-10-08 10:53:32 +0800

    Update App.js

--- App.js
+++ App.js
@@ -1,19 +1,53 @@
 import React from "react";
-import { Navbar, Nav } from "react-bootstrap";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom"; 
+import "./App.css"; 
+
+function Home() {
+  return (
+    <div className='home'>
+      <h1>Task Flow</h1>
+      <p>Task Management Application</p>
+    </div>
+  );
+}
+
+function AddTask() {
+  return (
+    <div className='addtask'>
+      <h1>This is a Task Management Application where you can add task.</h1>
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div>
-      <Navbar bg='light' expand='lg'>
-        <Navbar.Brand href='#home'>My App</Navbar.Brand>
-        <Navbar.Toggle aria-controls='basic-navbar-nav' />
-        <Navbar.Collapse id='basic-navbar-nav'>
-          <Nav className='mr-auto'>
-            <Nav.Link href='#home'>Home</Nav.Link>
-            <Nav.Link href='#add-task'>Add Task</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
+    <div className='app'>
+      {/* Set up Router for navigation */}
+      <Router>
+        {/* Simple Navigation Links */}
+        <nav className='navbar'>
+          <ul>
+            <li>
+              <Link to='/home' className='nav-link'>
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link to='/addtask' className='nav-link'>
+                Add Task
+              </Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* Define the routes */}
+        <Routes>
+          <Route path='/home' element={<Home />} />
+          <Route path='/addtask' element={<AddTask />} />
+        </Routes>
+      </Router>
+
+      {/* Optionally Include the FunctionalComponent */}
     </div>
   );
 }
