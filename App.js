0ba60826e0c8cd6105f7c353938602cdc6cb6ff8
reaganzz <gabuteloreagan@gmail.com>
2025-10-08 11:35:27 +0800

    Update App.js

--- App.js
+++ App.js
@@ -1,42 +1,101 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import "./App.css"; 
+import "./App.css";
 
-function TaskListView() {
+function TaskListView({ tasks, deleteTask }) {
   return (
-    <div className='task-list-view'>
+    <div className="task-list-view">
       <h1>Task List</h1>
-      <p>Here you can see all the tasks you have to manage.</p>
-      {/* Here, you would list the tasks in the future */}
+      {tasks.length === 0 ? (
+        <p>No tasks available. Please add a task.</p>
+      ) : (
+        <ul>
+          {tasks.map((task) => (
+            <li key={task.id}>
+              {task.title} - {task.description}{" "}
+              <button onClick={() => deleteTask(task.id)}>Delete</button>
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
 
-function AddTaskView() {
+function AddTaskView({ addTask }) {
+  const [taskDetails, setTaskDetails] = useState({
+    title: "",
+    description: "",
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setTaskDetails({ ...taskDetails, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newTask = { id: Date.now(), ...taskDetails }; 
+    addTask(newTask); 
+    setTaskDetails({ title: "", description: "" });
+  };
+
   return (
-    <div className='add-task-view'>
+    <div className="add-task-view">
       <h1>Add New Task</h1>
-      <p>This is where you can add a new task to your list.</p>
-      {/* Future form for adding tasks would go here */}
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Title: </label>
+          <input
+            type="text"
+            name="title"
+            value={taskDetails.title}
+            onChange={handleInputChange}
+            required
+          />
+        </div>
+        <div>
+          <label>Description: </label>
+          <input
+            type="text"
+            name="description"
+            value={taskDetails.description}
+            onChange={handleInputChange}
+            required
+          />
+        </div>
+        <button type="submit">Add Task</button>
+      </form>
     </div>
   );
 }
+
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+
+  const addTask = (taskDetails) => {
+    setTasks((prevTasks) => [...prevTasks, taskDetails]);
+  };
+
+  const deleteTask = (taskId) => {
+    setTasks((prevTasks) => prevTasks.filter((task) => task.id !== taskId));
+  };
+
   return (
-    <div className='app'>
+    <div className="App">
       {/* Set up Router for navigation */}
       <Router>
         {/* Simple Navigation Links */}
-        <nav className='navbar'>
+        <nav className="navbar">
           <ul>
             <li>
-              <Link to='/home' className='nav-link'>
+              <Link to="/home" className="nav-link">
                 Home
               </Link>
             </li>
             <li>
-              <Link to='/addtask' className='nav-link'>
+              <Link to="/addtask" className="nav-link">
                 Add Task
               </Link>
             </li>
@@ -45,8 +104,14 @@ function App() {
 
         {/* Define the routes */}
         <Routes>
-          <Route path='/home' element={<TaskListView />} />
-          <Route path='/addtask' element={<AddTaskView />} />
+          <Route
+            path="/home"
+            element={<TaskListView tasks={tasks} deleteTask={deleteTask} />}
+          />
+          <Route
+            path="/addtask"
+            element={<AddTaskView addTask={addTask} />}
+          />
         </Routes>
       </Router>
     </div>
@@ -54,3 +119,4 @@ function App() {
 }
 
 export default App;
+
